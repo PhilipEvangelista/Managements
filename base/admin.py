c2d0a7dad8bf78814d970561620ad40e8ffba2a4
PhilipEvangelista <philipevangelista732@gmail.com>
2022-04-03 13:59:05 +0800

    f

--- base/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-admin.site.register(models.Information)
-admin.site.register(models.History)
-admin.site.register(models.Security)
-admin.site.register(models.Bitcoin)
